ad95f4b3c2c0ae05700e61acb4f42b04370db821
aman <aman@ubuntu.(none)>
2012-11-04 00:26:23 +0530

    commented database_authenticable in migrations

--- db/migrate/20121102190047_devise_create_users.rb
+++ db/migrate/20121102190047_devise_create_users.rb
@@ -1,20 +1,16 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      #t.database_authenticatable :null => false
-      #t.confirmable
-      t.recoverable
-      t.rememberable
-      t.trackable
-      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-
+      ## Database authenticatable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+      ## Rememberable
+      t.datetime :remember_created_at
       t.timestamps
-    end
-
-    add_index :users, :email,                :unique => true
-    #add_index :users, :confirmation_token,   :unique => true
-    add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :unlock_token,         :unique => true
+    end    
   end
 
   def self.down
