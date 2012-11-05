fb1a7f2c7813e4f969b5ac7763a681810851bfcd
aman <aman@ubuntu.(none)>
2012-11-06 00:58:50 +0530

    copied rake tasks to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# "find_or_create_by_name Projects"  
+Project.find_or_create_by_name('First')
+Project.find_or_create_by_name('Second')
+Project.find_or_create_by_name('Third')
+Project.find_or_create_by_name('Fourth')
+Project.find_or_create_by_name('Fifth')
+Project.find_or_create_by_name('Others')
+  
+
+ # desc "find_or_create_by_name Task Types"  
+TaskType.find_or_create_by_name('Analysis')
+TaskType.find_or_create_by_name('Coding')
+TaskType.find_or_create_by_name('Documentaion')
+TaskType.find_or_create_by_name('Research')
+TaskType.find_or_create_by_name('Training')
+TaskType.find_or_create_by_name('Others')  
